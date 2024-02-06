6708c40f32ad5722b3b4f4c37afc208b934d77ac
Diego Nacimiento <diegonacimiento18@gmail.com>
2024-02-06 19:29:55 -0300

    feat: finished about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ const About = () => {
   return (
     <div className="flex flex-col leading-8 w-full">
       <section className="bg-secondary p-4 text-primary">
-        <div className="max-w-2k m-auto px-4">
+        <div className="max-w-2k m-auto px-4 py-10">
           <h3 className="text-2xl font-semibold">About</h3>
           <p className="text-justify my-4">
             Explore our innovative virtual shopping space! Immerse yourself in a
@@ -48,78 +48,90 @@ const About = () => {
       </section>
 
       <section className="bg-primary p-4 text-secondary">
-        <div className="max-w-2k m-auto px-4">
-        <div className="my-4">
-          <h3 className="text-2xl font-semibold">Vision</h3>
-          <p className="text-justify font-light my-2">
-            To be the leading ecommerce platform, recognized for delivering an
-            exceptional shopping experience and for connecting consumers with
-            innovative and quality products from globally renowned brands.
-          </p>
-        </div>
-        <div className="my-4">
-          <h3 className="text-2xl font-semibold">Mission</h3>
-          <p className="text-justify font-light my-2">
-            Our mission is to offer a seamless, secure, and enjoyable online
-            shopping experience by curating a diverse range of high-quality
-            products. We prioritize customer satisfaction through exceptional
-            service, data security, and privacy standards, aiming to become a
-            trusted ecommerce platform fostering innovation and trust.
-          </p>
-        </div>
+        <div className="flex flex-col gap-8 max-w-2k m-auto px-4 py-10">
+          <div className="my-4">
+            <h3 className="text-2xl font-semibold">Vision</h3>
+            <p className="text-justify font-light my-2">
+              To be the leading ecommerce platform, recognized for delivering an
+              exceptional shopping experience and for connecting consumers with
+              innovative and quality products from globally renowned brands.
+            </p>
+          </div>
+          <div className="my-4">
+            <h3 className="text-2xl font-semibold">Mission</h3>
+            <p className="text-justify font-light my-2">
+              Our mission is to offer a seamless, secure, and enjoyable online
+              shopping experience by curating a diverse range of high-quality
+              products. We prioritize customer satisfaction through exceptional
+              service, data security, and privacy standards, aiming to become a
+              trusted ecommerce platform fostering innovation and trust.
+            </p>
+          </div>
         </div>
       </section>
 
       <section className="p-4">
-        <div className="max-w-2k m-auto px-4">
+        <div className="max-w-2k m-auto px-4 py-10">
           <h3 className="text-2xl font-semibold">Technologies</h3>
           <div className="grid grid-cols-2 sm:grid-cols-3 gap-12 justify-items-center items-center my-4">
-            <Image
-              width={100}
-              height={100}
-              src={"https://i.imgur.com/xr3xQTa.png"}
-              alt="Nest.js icon"
-              title="Nest.js icon"
-            />
-            <Image
-              width={100}
-              height={100}
-              src={"https://i.imgur.com/hffY0kC.png"}
-              alt="TypeScript icon"
-              title="TypeScript icon"
-            />
+            <span className="h-24 w-24">
+              <Image
+                width={100}
+                height={100}
+                src={"https://i.imgur.com/xr3xQTa.png"}
+                alt="Nest.js icon"
+                title="Nest.js icon"
+              />
+            </span>
+            <span className="h-24 w-24">
+              <Image
+                width={100}
+                height={100}
+                src={"https://i.imgur.com/hffY0kC.png"}
+                alt="TypeScript icon"
+                title="TypeScript icon"
+              />
+            </span>
 
-            <Image
-              width={100}
-              height={100}
-              src={"https://i.imgur.com/qEzAmez.png"}
-              alt="TypeORM icon"
-              title="TypeORM icon"
-            />
+            <span className="h-24 w-24">
+              <Image
+                width={100}
+                height={100}
+                src={"https://i.imgur.com/qEzAmez.png"}
+                alt="TypeORM icon"
+                title="TypeORM icon"
+              />
+            </span>
 
-            <Image
-              width={100}
-              height={100}
-              src={"https://i.imgur.com/rydGd27.png"}
-              alt="PostgreSQL icon"
-              title="PostgreSQL icon"
-            />
+            <span className="h-24 w-24">
+              <Image
+                width={100}
+                height={100}
+                src={"https://i.imgur.com/rydGd27.png"}
+                alt="PostgreSQL icon"
+                title="PostgreSQL icon"
+              />
+            </span>
 
-            <Image
-              width={100}
-              height={100}
-              src={"https://i.imgur.com/5ScobMw.png"}
-              alt="Next.js icon"
-              title="Next.js icon"
-            />
+            <span className="h-24 w-24">
+              <Image
+                width={100}
+                height={100}
+                src={"https://i.imgur.com/5ScobMw.png"}
+                alt="Next.js icon"
+                title="Next.js icon"
+              />
+            </span>
 
-            <Image
-              width={100}
-              height={100}
-              src={"https://i.imgur.com/mB4MUGf.png"}
-              title="TailwindCSS icon"
-              alt="TailwindCSS icon"
-            />
+            <span className="h-24 w-24">
+              <Image
+                width={100}
+                height={100}
+                src={"https://i.imgur.com/mB4MUGf.png"}
+                title="TailwindCSS icon"
+                alt="TailwindCSS icon"
+              />
+            </span>
           </div>
         </div>
       </section>
