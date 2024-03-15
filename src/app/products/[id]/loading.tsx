49e218a4a9a2f05df331d2345be40dd02f26d01b
Diego Nacimiento <diegonacimiento18@gmail.com>
2024-03-14 21:26:07 -0300

    feat: changes in styles loadings products

--- src/app/products/[id]/loading.tsx
+++ src/app/products/[id]/loading.tsx
@@ -3,21 +3,20 @@ import LoadingProducts from "../LoadingProducts";
 
 const loading = () => {
   return (
-    <div className="h-full w-full sm:px-4 max-w-6xl">
-
-      <div className="flex flex-col overflow-hidden sm:flex-row min-h-600p w-full gap-12 px-4 py-8 m-auto">
+    <div className="h-full w-full px-4 max-w-6xl">
+      <div className="flex flex-col overflow-hidden sm:flex-row min-h-600p w-full gap-12 sm:px-4 py-8 m-auto">
         <figure
           className={
             "relative w-full duration-150 h-[25rem] sm:h-[34.375rem] bg-secondary bg-opacity-35 animate-pulse"
           }
         ></figure>
-        <div className="flex flex-col gap-6 w-full">
+        <div className="flex flex-col gap-6 px-4 w-full">
           <h1
             className={
               "text-lg font-bold px-4 h-14 rounded-xl bg-secondary bg-opacity-35 animate-pulse"
             }
           ></h1>
-          <p className="text-lg font-semibold px-4 h-7 w-30 rounded-xl bg-secondary bg-opacity-35 animate-pulse"></p>
+          <p className="text-lg font-semibold h-7 w-30 rounded-xl bg-secondary bg-opacity-35 animate-pulse"></p>
 
           <div className="flex items-center justify-between gap-4">
             <div className="h-10 w-30 rounded-xl bg-secondary bg-opacity-35 animate-pulse"></div>
@@ -26,7 +25,10 @@ const loading = () => {
           </div>
           <hr />
           <h3 className="h-7 w-full rounded-xl bg-secondary bg-opacity-35 animate-pulse"></h3>
-          <p className="h-18 w-full rounded-xl bg-secondary bg-opacity-35 animate-pulse"></p>
+          <span className="px-2">
+            {" "}
+            <p className="h-18 w-full rounded-xl bg-secondary bg-opacity-35 animate-pulse"></p>
+          </span>
         </div>
       </div>
 
@@ -35,7 +37,6 @@ const loading = () => {
       <h3 className="sm:p-4 text-2xl font-medium">Related products</h3>
 
       <LoadingProducts length={6} />
-      
     </div>
   );
 };
