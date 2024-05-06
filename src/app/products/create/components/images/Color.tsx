7794bb940b6cafff5fa13044eb1efe589e0ae1e1
Diego Nacimiento <diegonacimiento18@gmail.com>
2024-05-06 10:27:45 -0300

    feat: advanced color component in images

--- src/app/products/create/components/images/Color.tsx
+++ src/app/products/create/components/images/Color.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 import { IPayload } from "../Form";
 import { IProductImage } from "@/types/products";
+import { mainColors } from "@/utils/temporal";
+import { formStyles } from "../../styles/FormStyles";
+import { MdOutlineClose } from "react-icons/md";
 
 interface IColorProps {
   images: IPayload["images"];
@@ -14,32 +18,58 @@ const Color: React.FC<IColorProps> = ({
   currentImage,
   handleSelectedColor,
 }) => {
+  const [showColorList, setShowColorList] = useState<boolean>(false);
+
+  const toggleColorList = () => {
+    setShowColorList((prev) => !prev);
+  };
+
+  const colorsProduct = images.value.map((image) => image.color);
+
+  const colors = showColorList
+    ? mainColors.filter((color) => !colorsProduct.includes(color))
+    : colorsProduct;
+
   return (
-    <div className="flex gap-2 p-2 rounded-md bg-primary">
+    <div className={formStyles.container}>
+      <label className={formStyles.label}>Colors</label>
 
-      {images.value.map((image) => (
-        <div
-          style={{ backgroundColor: image.color }}
-          key={image.color}
-          onClick={() => handleSelectedColor(image.color)}
-          className={
-            "rounded-full h-8 w-8 cursor-pointer " +
-            (image.color === currentImage.color
-              ? " border-2 border-secondary"
-              : "opacity-70 hover:opacity-100 hover:border-2 hover:border-body duration-150")
-          }
-        ></div>
-      ))}
+      <div className="flex gap-4 flex-wrap p-4 rounded-md bg-primary">
+        {showColorList && (
+          <button
+            type="button"
+            title="Close"
+            onClick={toggleColorList}
+            className="flex items-center justify-center border border-red-600 h-8 w-8 rounded-full hover:scale-105 duration-150"
+          >
+            <MdOutlineClose />
+          </button>
+        )}
+        {colors.map((color) => (
+          <div
+            style={{ backgroundColor: color }}
+            key={color}
+            onClick={() => handleSelectedColor(color)}
+            className={
+              "rounded-full h-8 w-8 cursor-pointer " +
+              (color === currentImage.color
+                ? " border-2 border-secondary"
+                : "opacity-70 hover:opacity-100 hover:border-2 hover:border-body duration-150")
+            }
+          ></div>
+        ))}
 
-      {images.value.length < 9 && (
-        <button
-          type="button"
-          title="Add color"
-          className="rounded-full h-8 w-8 bg-gray-300 text-white text-xl opacity-70 hover:opacity-100 hover:border-2 hover:border-body duration-150"
-        >
-          +
-        </button>
-      )}
+        {images.value.length < 9 && !showColorList && (
+          <button
+            type="button"
+            title="Add color"
+            onClick={toggleColorList}
+            className="rounded-full h-8 w-8 bg-gray-300 text-white text-xl opacity-70 hover:opacity-100 hover:border-2 hover:border-body duration-150"
+          >
+            +
+          </button>
+        )}
+      </div>
     </div>
   );
 };
