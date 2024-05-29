45b8c0b926286eb400c041c775b0544c629a1145
Diego Nacimiento <diegonacimiento18@gmail.com>
2024-05-29 14:14:54 -0300

    feat: loading main

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const loading = () => {
   return (
-    <div>loading</div>
-  )
-}
+    <div className=" grid place-content-center p-4 h-full min-h-screen w-full bg-primary bg-opacity-45 text-secondary">
+      <div className="relative flex flex-col gap-4 justify-center items-center h-full w-full">
+        <h1 className="text-center text-2xl font-bold antialiased font-mono tracking-widest">
+          Velmar
+        </h1>
+        <div className="flex gap-1">
+          <div className="h-5 w-5 bg-secondary bg-opacity-80 animate-bounce [animation-delay:-0.3s] rounded-full"></div>
+          <div className="h-5 w-5 bg-secondary bg-opacity-80 animate-bounce [animation-delay:-0.15s] rounded-full"></div>
+          <div className="h-5 w-5 bg-secondary bg-opacity-80 animate-bounce rounded-full"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default loading
+export default loading;
