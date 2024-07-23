392fc39ba92e7ac69f2bb4c8f47d7176f4af51d1
Diego Nacimiento <diegonacimiento18@gmail.com>
2024-07-23 17:45:27 -0300

    feat: added contacts links

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,37 +2,48 @@ import React from "react";
 import { FaBriefcase, FaLinkedin, FaTwitter } from "react-icons/fa";
 import { FaSheetPlastic } from "react-icons/fa6";
 
+import { cv, linkedin, portfolio, twitter } from "./utils/contacts";
+
 const Contact = () => {
   const divStyles = "h-60 w-full bg-primary";
 
-  const aStyles = "flex flex-col justify-center items-center p-4 border-t border-secondary h-full w-full max-w-96 m-auto text-3xl hover:opacity-45 hover:bg-secondary hover:text-primary duration-150";
+  const aStyles =
+    "flex flex-col justify-center items-center p-4 border-t border-secondary h-full w-full max-w-96 m-auto text-3xl hover:opacity-45 hover:bg-secondary hover:text-primary duration-150";
 
   const pStyles = "py-3 text-lg text-center";
 
   return (
     <div className="pt-10 w-full leading-8 text-secondary">
       <h2 className="text-2xl text-center font-bold">Contact me</h2>
-      <p className="max-w-2k mt-8 mb-16 m-auto px-8 text-justify">Hello there! Thank you for visiting my social networks. I&apos;m currently actively seeking new job opportunities and would love to connect with you to discuss code and potential collaborations on interesting projects. Whether you have an opportunity available or just want to chat about technology, feel free to reach out! I&apos;m open to new ideas and excited about what the future may hold. Looking forward to hearing from you soon!</p>
+      <p className="max-w-2k mt-8 mb-16 m-auto px-8 text-justify">
+        Hello there! Thank you for visiting my social networks. I&apos;m
+        currently actively seeking new job opportunities and would love to
+        connect with you to discuss code and potential collaborations on
+        interesting projects. Whether you have an opportunity available or just
+        want to chat about technology, feel free to reach out! I&apos;m open to
+        new ideas and excited about what the future may hold. Looking forward to
+        hearing from you soon!
+      </p>
       <div className={divStyles}>
-        <a className={aStyles + " border-t-0"} href="/">
+        <a target="_blank" className={aStyles + " border-t-0"} href={linkedin}>
           <FaLinkedin />
-        <p className={pStyles}>Diego Nacimiento</p>
+          <p className={pStyles}>Diego Nacimiento</p>
         </a>
       </div>
       <div className={divStyles}>
-        <a className={aStyles} href="/">
+        <a target="_blank" className={aStyles} href={twitter}>
           <FaTwitter />
           <p className={pStyles}>@diegonac23</p>
         </a>
       </div>
       <div className={divStyles}>
-        <a className={aStyles} href="/">
+        <a target="_blank" className={aStyles} href={portfolio}>
           <FaBriefcase />
           <p className={pStyles}>My portfolio</p>
         </a>
       </div>
       <div className={divStyles}>
-        <a className={aStyles} href="/">
+        <a target="_blank" className={aStyles} href={cv}>
           <FaSheetPlastic />
           <p className={pStyles}>CV</p>
         </a>
---
+++ src/app/contact/utils/contacts.ts
@@ -0,0 +1,9 @@
+export const linkedin =
+  "https://www.linkedin.com/in/diego-nacimiento-01b2b025a";
+
+export const twitter = "https://twitter.com/diegonac23";
+
+export const portfolio = "https://portfolio-diegonacimiento.vercel.app";
+
+export const cv =
+  "https://www.dropbox.com/scl/fi/gt0g1ca46g5m4erz6h6lk/Full-Stack-Developer-Diego-Nacimiento.pdf?rlkey=7p4kxy77rq1z4b7o8sv5pe7qy&st=ve7a3ikp&dl=0";
